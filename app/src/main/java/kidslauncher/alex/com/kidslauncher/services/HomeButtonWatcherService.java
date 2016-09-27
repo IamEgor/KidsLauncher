fd5bec93edeed74917cb6f550876b09067f35ef2
Yegorka <e.gruk@softteco.com>
2016-09-27 20:14:20 +0300

    fix rare bug with not relaunch

--- app/src/main/java/kidslauncher/alex/com/kidslauncher/services/HomeButtonWatcherService.java
+++ app/src/main/java/kidslauncher/alex/com/kidslauncher/services/HomeButtonWatcherService.java
@@ -8,6 +8,10 @@ import android.os.IBinder;
 import android.support.annotation.Nullable;
 import android.util.Log;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
+import kidslauncher.alex.com.kidslauncher.KidsLauncherApp;
 import kidslauncher.alex.com.kidslauncher.ui.activities.HomeActivity;
 import kidslauncher.alex.com.kidslauncher.utils.HomeButtonWatcher;
 import kidslauncher.alex.com.kidslauncher.utils.OnHomePressedListener;
@@ -17,6 +21,7 @@ public class HomeButtonWatcherService extends Service {
 
     private static final int AFTER_LONG_PRESS_ON_HOME_BUTTON = 500;
     private static final int DEFAULT_START_ACTIVITY_DELAY = 5001;
+    private static final int CHECKER_DELAY = 10 * 1000;
     private static final String LOG_TAG = HomeButtonWatcherService.class.getName();
 
     private MyBinder binder = new MyBinder();
@@ -31,28 +36,25 @@ public class HomeButtonWatcherService extends Service {
 
                 @Override
                 public void onHomePressed() {
-                    Log.w(LOG_TAG, "HomeButtonWatcher.onHomePressed() \n" +
-                            "needToRestartActivity = " + needToRestartActivity());
-                    if (needToRestartActivity()) {
-                        restartActivity(DEFAULT_START_ACTIVITY_DELAY);
-                    }
+                    Log.w(LOG_TAG, "HomeButtonWatcher.onHomePressed() \n" + "needToRestartActivity = " + needToRestartActivity());
+                    restartActivityIfNeeded(DEFAULT_START_ACTIVITY_DELAY);
                 }
 
                 @Override
                 public void onHomeLongPressed() {
-                    Log.w(LOG_TAG, "HomeButtonWatcher.onHomeLongPressed() \n" +
-                            "needToRestartActivity = " + needToRestartActivity());
-                    if (needToRestartActivity()) {
-                        restartActivity(AFTER_LONG_PRESS_ON_HOME_BUTTON);
-                    }
+                    Log.w(LOG_TAG, "HomeButtonWatcher.onHomeLongPressed() \n" + "needToRestartActivity = " + needToRestartActivity());
+                    restartActivityIfNeeded(AFTER_LONG_PRESS_ON_HOME_BUTTON);
                 }
 
                 @Override
                 public void onGoogleHelperCall() {
-                    Log.w(LOG_TAG, "HomeButtonWatcher.onGoogleHelperCall() \n" +
-                            "needToRestartActivity = " + needToRestartActivity());
+                    Log.w(LOG_TAG, "HomeButtonWatcher.onGoogleHelperCall() \n" + "needToRestartActivity = " + needToRestartActivity());
+                    restartActivityIfNeeded(AFTER_LONG_PRESS_ON_HOME_BUTTON);
+                }
+
+                private void restartActivityIfNeeded(int delay) {
                     if (needToRestartActivity()) {
-                        restartActivity(AFTER_LONG_PRESS_ON_HOME_BUTTON);
+                        restartActivity(delay);
                     }
                 }
 
@@ -62,6 +64,8 @@ public class HomeButtonWatcherService extends Service {
             });
             mHomeButtonWatcher.startWatch();
             restartActivity();
+            Timer timer = new Timer();
+            timer.schedule(new ForegroundChecker(), CHECKER_DELAY, CHECKER_DELAY);
         } else {
             PreferencesUtil.getInstance().setCloseAllowed(false);
             stopSelf();
@@ -85,14 +89,14 @@ public class HomeButtonWatcherService extends Service {
     }
 
     public void restartActivity(int delay) {
-        new Handler().postDelayed(this::restartActivity, delay);
+        new Handler().postDelayed(HomeButtonWatcherService::restartActivity, delay);
     }
 
-    public void restartActivity() {
-        Intent i = new Intent(this, HomeActivity.class);
+    public static void restartActivity() {
+        Intent i = new Intent(KidsLauncherApp.getInstance(), HomeActivity.class);
         i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
         i.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-        startActivity(i);
+        KidsLauncherApp.getInstance().startActivity(i);
         Log.d(LOG_TAG, LOG_TAG + " restartActivity");
     }
 
@@ -101,4 +105,21 @@ public class HomeButtonWatcherService extends Service {
             return HomeButtonWatcherService.this;
         }
     }
+
+    static class ForegroundChecker extends TimerTask {
+
+        private static final String TAG = ForegroundChecker.class.getSimpleName();
+
+        @Override
+        public void run() {
+            if (needToRestartActivity()) {
+                restartActivity();
+            }
+        }
+
+        private boolean needToRestartActivity() {
+            return !PreferencesUtil.getInstance().isAppInForeground() && !PreferencesUtil.getInstance().isCloseAllowed();
+        }
+    }
+
 }
